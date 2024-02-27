680f29d02bd004ca62f31408ba7da4be5431e0b6
Mattkloz <mjklosinski@gmail.com>
2024-02-27 11:32:23 -0500

    format and icon updates

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Quasar App (adhd-viewer)
-
-A Quasar Project
+# MyADHDReader
 
 ## Install the dependencies
+
 ```bash
 yarn
 # or
@@ -10,32 +9,41 @@ npm install
 ```
 
 ### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
 ```bash
-quasar dev
+yarn start
 ```
 
-
 ### Lint the files
+
 ```bash
 yarn lint
 # or
 npm run lint
 ```
 
-
 ### Format the files
+
 ```bash
 yarn format
 # or
 npm run format
 ```
 
-
-
 ### Build the app for production
+
 ```bash
-quasar build
+yarn build
 ```
 
+### Add the extension to Chrome or Firefox
+
+_Build the project first_
+
+- Navigate to Chrome>Preferences>Extensions
+- Click 'Load Unpacked' at the top
+- Navigate to the dist>bex folder in the MyADHDViewed directory
+
 ### Customize the configuration
+
 See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
--- src-bex/dom/sift.ts
+++ src-bex/dom/sift.ts
@@ -1,100 +1,54 @@
 function getRandomInt(max: number, min: number) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
+  // Create a random number between the min and max
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function paragraphStyle(p: HTMLParagraphElement) {
+  p.style.fontSize = '1.15rem';
+  p.style.letterSpacing = '1.3px';
+  p.style.lineHeight = '1.75';
+  p.style.fontWeight = '200';
+  p.style.color = '#434343';
+}
+
+function makeNewWords(words: string[]): string[] {
+  // skipCount is a randomized number of words to skip before replacing
+  let skipCount = getRandomInt(6, 2);
+  const newWords = words.map((word, index) => {
+    if (index + 1 < skipCount) return word;
+    const wordLength = word.length;
+    // determine where to split the word based on its length
+    let splitAt = 0;
+    if (wordLength < 3) {
+      if (wordLength === 1) splitAt = 1;
+      splitAt = wordLength - 1;
+    } else {
+      splitAt = getRandomInt(4, 2);
+    }
+    const splitWord = word.slice(0, splitAt);
+    // create the new word with the split part wrapped in a span to add styling
+    const replaced = `<span class="replaced-str" style="font-weight: 700; color: #000;">${splitWord}</span>`;
+    skipCount = skipCount + getRandomInt(6, 2);
+    // return the word with the split part replaced
+    return word.replace(splitWord, replaced);
+  });
+  return newWords;
 }
 
 export default function siftPage() {
   if (window.location.hostname !== 'en.wikipedia.org') return;
   const paragraphs = document.querySelectorAll('p') as NodeListOf<HTMLParagraphElement>;
   paragraphs.forEach((p) => {
+    // if the paragraph is too short, skip it
     if (p.innerHTML.length < 10) return;
-    p.style.fontSize = '1.15rem';
-    p.style.letterSpacing = '1.3px';
-    p.style.lineHeight = '1.75';
-    p.style.fontWeight = '200';
-    p.style.color = '#434343';
+    paragraphStyle(p);
     const text = p.textContent as string;
+    // split the paragraph into an array of words
     const words = text.split(' ');
-    let thisCount = getRandomInt(6, 2);
-
-    const newWords = words.map((word, index) => {
-      if (index + 1 < thisCount) return word;
-      const wordLength = word.length;
-      let splitAt = 0;
-      if (wordLength < 3) {
-        if (wordLength === 1) splitAt = 1;
-        splitAt = wordLength - 1;
-      } else {
-        splitAt = getRandomInt(4, 2);
-      }
-      const splitWord = word.slice(0, splitAt);
-      const replaced = `<span class="replaced-str" style="font-weight: 700; color: #000;">${splitWord}</span>`;
-      thisCount = thisCount + getRandomInt(6, 2);
-      return word.replace(splitWord, replaced);
-    });
+    const newWords = makeNewWords(words);
     const newHTML = newWords.join(' ');
     p.innerHTML = newHTML;
-
-    //   if (word.includes('<span class="replaced-str" style="font-weight: 700">')) return;
-    //   if (word.includes('<') && word.includes('>')) {
-    //     const find1 = [...word.matchAll(/</g)];
-    //     const justValues = find1.map((match) => match.index) as number[];
-    //     const highest1 = Math.max(...justValues);
-    //     const find2 = [...word.matchAll(/>/g)];
-    //     const justValues2 = find2.map((match) => match.index) as number[];
-    //     const highest2 = Math.max(...justValues2);
-    //     if (highest1 > highest2) {
-    //       hasOpenTag = true;
-    //       return;
-    //     } else {
-    //       hasOpenTag = false;
-    //       return;
-    //     }
-    //   }
-    //   if (word.includes('<')) {
-    //     hasOpenTag = true;
-    //     return;
-    //   }
-
-    //   if (word.includes('>')) {
-    //     hasOpenTag = false;
-    //     return;
-    //   }
-    //   if (hasOpenTag) {
-    //     return;
-    //   }
-    //   if (word.includes('href') || word.includes('title=') || word.includes('id')) {
-    //     return;
-    //   }
-    //   if (sinceLastIndex < thisCount) {
-    //     sinceLastIndex++;
-    //     return;
-    //   } else if (sinceLastIndex === thisCount) {
-
-    //     const wordLength = word.length;
-    //     let splitAt = 0;
-    //     if (wordLength < 3) {
-    //       if (wordLength === 1) splitAt = 1;
-    //       splitAt = wordLength - 1;
-    //     } else {
-    //       splitAt = getRandomInt(4, 1);
-    //     }
-    //     const splitWord = word.slice(0, splitAt);
-    //     // console.log(JSON.parse(JSON.stringify(splitWord)));
-    //     // console.log(JSON.parse(JSON.stringify(word)));
-    //     // console.log(JSON.parse(JSON.stringify(index)));
-    //     const newWord = `<span class="replaced-str" style="font-weight: 700">${splitWord}</span>`;
-    //     const combWord = word.replace(splitWord, newWord);
-    //     p.innerHTML = p.innerHTML.replace(word, combWord);
-    //     // console.log(p.innerHTML);
-    //     sinceLastIndex = 1;
-    //     const genCount = getRandomInt(6, 3);
-    //     thisCount = genCount;
-    //     return;
-    //   }
-    // });
-  }
-
-  );
+  });
 }
